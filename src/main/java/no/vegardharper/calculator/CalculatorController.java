541ce211bf34bde116315e13502db9e3485d19ac
Vegard Harper <vegard.harper@gmail.com>
2018-05-28 23:40:44 +0200

    Switched annotation...

--- src/main/java/no/vegardharper/calculator/CalculatorController.java
+++ src/main/java/no/vegardharper/calculator/CalculatorController.java
@@ -1,11 +1,11 @@
 package no.vegardharper.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CalculatorController {
 
 	@Autowired
