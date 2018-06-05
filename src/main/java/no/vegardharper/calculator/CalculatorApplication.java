25fb7ea79c0811daba26add1c20aeafd71682b55
Vegard Harper <vegard.harper@gmail.com>
2018-06-05 21:24:21 +0200

    Cache values in redis

--- src/main/java/no/vegardharper/calculator/CalculatorApplication.java
+++ src/main/java/no/vegardharper/calculator/CalculatorApplication.java
@@ -2,11 +2,21 @@ package no.vegardharper.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @SpringBootApplication
+@EnableCaching
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+	@Bean
+	public LettuceConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(new RedisStandaloneConfiguration("redis", 6379));
 	}
 }
--- src/main/java/no/vegardharper/calculator/CalculatorController.java
+++ src/main/java/no/vegardharper/calculator/CalculatorController.java
@@ -1,6 +1,7 @@
 package no.vegardharper.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,11 +14,13 @@ public class CalculatorController {
 
 
 	@RequestMapping("sum")
+	@Cacheable("sum")
 	public String sum(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
 		return String.valueOf(calculator.sum(a, b));
 	}
 
 	@RequestMapping("subtract")
+	@Cacheable("subtract")
 	public String subtract(@RequestParam("a") Integer a, @RequestParam("b") Integer b) {
 		return String.valueOf(calculator.subtract(a, b));
 	}
